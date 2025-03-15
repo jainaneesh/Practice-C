c33f311662f31cb76b2ebbe31b53ff8be08a5a5c
jainaneesh <jainaneesh15@gmail.com>
2025-03-15 17:04:31 -0400

    Completed this program understanding what each system call does

--- ForInterview/FILEIO.c
+++ ForInterview/FILEIO.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 
+#define STORAGE_BUFFER_SIZE 50
 #define FILE "open_this_file.txt"
 
 int main(){
 	int file_descriptor = -1;
-	char buffer_to_store_data[100];
+	char buffer_to_store_data[STORAGE_BUFFER_SIZE];
 
 	file_descriptor = open(FILE, O_RDONLY | O_WRONLY | O_CREAT, 0600);
 	if (file_descriptor == -1){
@@ -20,7 +21,7 @@ int main(){
 	}
 
 	const char* write_to_file = "Hello from outside the file\n\r";
-	size_t bytes_written = write(file_descriptor,write_to_file,strlen(write_to_file));
+	ssize_t bytes_written = write(file_descriptor,write_to_file,strlen(write_to_file));
 	
 	if (bytes_written == -1){
 		perror("Error writing to file");
@@ -31,5 +32,28 @@ int main(){
 		printf("Write successful, bytes written %ld\r\n",bytes_written);
 	}
 	close(file_descriptor);
+
+	file_descriptor = open(FILE, O_RDONLY);
+
+	if (file_descriptor == -1){
+		perror("Error opening file");
+		return 1;
+	}
+
+	ssize_t bytes_read = read(file_descriptor, buffer_to_store_data, sizeof(buffer_to_store_data)-1);
+
+	if (bytes_read == -1){
+		perror("Error reading file");
+		close(file_descriptor);
+		return 1;
+	}
+
+	buffer_to_store_data[bytes_read] = '\0';
+
+	printf("File content: %s\r\n",buffer_to_store_data);
+
+	close(file_descriptor);
+
+	return 0;
 
 }
